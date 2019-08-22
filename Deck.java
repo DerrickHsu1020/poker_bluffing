d05fb037a2e71cdf9a3c42c89025de8f0549be93
DerrickHsu1020 <derrickhsu1020@gmail.com>
2019-08-22 10:41:20 +0100

    Add files via upload

---
+++ Deck.java
@@ -0,0 +1,122 @@
+package poker_blufffing;
+import java.util.ArrayList;
+import java.util.HashSet;
+public class Deck {
+	private HashSet<String> deck = new HashSet<>();
+	private static String colour[] = {"s", "h", "d", "c"};
+	public Deck(){
+			deck.add("As"); deck.add("Ac"); deck.add("Ad"); deck.add("Ah"); 
+			deck.add("Ks"); deck.add("Kc"); deck.add("Kd"); deck.add("Kh");
+			deck.add("Qs"); deck.add("Qc"); deck.add("Qd"); deck.add("Qh");
+			deck.add("Js"); deck.add("Jc"); deck.add("Jd"); deck.add("Jh");
+			deck.add("Ts"); deck.add("Tc"); deck.add("Td"); deck.add("Th");
+			deck.add("9s"); deck.add("9c"); deck.add("9d"); deck.add("9h");
+			deck.add("8s"); deck.add("8c"); deck.add("8d"); deck.add("8h");
+			deck.add("7s"); deck.add("7c"); deck.add("7d"); deck.add("7h");
+			deck.add("6s"); deck.add("6c"); deck.add("6d"); deck.add("6h");
+			deck.add("5s"); deck.add("5c"); deck.add("5d"); deck.add("5h");
+			deck.add("4s"); deck.add("4c"); deck.add("4d"); deck.add("4h");
+			deck.add("3s"); deck.add("3c"); deck.add("3d"); deck.add("3h");
+			deck.add("2s"); deck.add("2c"); deck.add("2d"); deck.add("2h");
+		}
+	public void remove(String s) {
+		String card="";
+		for(int i=0; i<s.length();i++) {
+	       	char c=s.charAt(i);
+	       	if ((c=='s')||(c=='d')||(c=='c')||(c=='h')) {
+	       		card=card+s.charAt(i-1)+c;
+	       		if (deck.contains(card)) {
+	       			deck.remove(card);
+	       			card="";
+	       		}
+	       	}
+	    }
+		 
+	}
+	public String getdeck() {
+		return deck.toString();
+	}
+	
+	public double bluffing(ArrayList <String> range, String community) {
+		int bluffing=0;
+		int total=0;
+		rank com=new rank(community);
+		for (int i=0;i<range.size();i++) {
+			String s = range.get(i);
+			if (s.charAt(0)==s.charAt(1)) {
+				char c=s.charAt(0);
+				ArrayList <String> combination = new ArrayList<>();
+				for (int j=0;j<colour.length;j++) {
+					String card=c+colour[j];
+					if (deck.contains(card)) {
+						combination.add(card);
+					}
+				}
+				if (combination.size()>1) {
+					for (int j=0;j<combination.size()-1;j++) {
+						for (int k=0;k<combination.size();k++) {
+							String hand=combination.get(j)+combination.get(k);
+							rank r=new rank(hand+community);
+							if (com.getrank()==r.getrank()) {
+								bluffing++;
+								total++;
+							}else {
+								total++;
+							}
+						}
+					}
+				}
+			}else if(s.charAt(2)=='s') {
+				char c1=s.charAt(0);
+				char c2=s.charAt(1);
+				for (int j=0;j<colour.length;j++) {
+					String card =""+c1+colour[j];
+					String card2=""+c2+colour[j];
+					if (deck.contains(card)&&deck.contains(card2)) {
+						String hand=card+card2;
+						rank r=new rank(hand+community);
+						if (com.getrank()==r.getrank()) {
+							bluffing++;
+							total++;
+						}else {
+							total++;
+						}
+					}
+				}
+			}else if(s.charAt(2)=='o') {
+				char c1=s.charAt(0);
+				char c2=s.charAt(1);
+				ArrayList <String> combination = new ArrayList<>();
+				for (int j=0;j<colour.length;j++) {
+					String card =""+c1+colour[j];
+					String card2=""+c2+colour[j];
+					if (deck.contains(card)) 
+						combination.add(card);
+					if (deck.contains(card2))
+						combination.add(card2);
+				}
+				if (combination.size()>1) {
+					for (int j=0;j<combination.size()-1;j++) {
+						for (int k=j+1;k<combination.size();k++) {
+							String card=combination.get(j);
+							String card2=combination.get(k);
+							if (card.charAt(1)!=card2.charAt(1)&&card.charAt(0)!=card2.charAt(0)) {
+								String hand=card+card2;
+								System.out.println(hand);
+								rank r=new rank(hand+community);
+								System.out.println(r.getrank());
+								if (com.getrank()==r.getrank()) {
+									bluffing++;
+									total++;
+								}else {
+									total++;
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+		return (double) bluffing*100/total;
+	}
+}
---
+++ Pokerbluffing.java
@@ -0,0 +1,184 @@
+package poker_blufffing;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import java.awt.Container;
+
+import javax.swing.JTable;
+import javax.swing.JCheckBox;
+import javax.swing.JToggleButton;
+import java.awt.GridLayout;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import javax.swing.JSplitPane;
+import java.awt.Dimension;
+import javax.swing.UIManager;
+import javax.swing.border.LineBorder;
+import java.awt.Color;
+import javax.swing.JTextPane;
+import javax.swing.ButtonGroup;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.ChangeEvent;
+import javax.swing.AbstractAction;
+import javax.swing.AbstractButton;
+
+import java.awt.event.ActionEvent;
+import javax.swing.Action;
+import java.awt.event.ItemListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionAdapter;
+
+public class Pokerbluffing {
+	private static String rank[] = {"A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"};
+	JFrame frmPokerbluffing;
+	private JToggleButton range;
+	private JTextField textField, player, board;
+	public ArrayList <String> s;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Pokerbluffing window = new Pokerbluffing();
+					window.frmPokerbluffing.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the application.
+	 */
+	public Pokerbluffing() {
+		frmPokerbluffing = new JFrame();
+		frmPokerbluffing.setTitle("PokerBluffing");
+		frmPokerbluffing.setBounds(100, 100, 450, 300);
+		frmPokerbluffing.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmPokerbluffing.getContentPane().setLayout(new BorderLayout(0, 0));
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		JPanel panel = new JPanel();
+		Font font1 = new Font("SansSerif", Font.BOLD, 20);
+		panel.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panel.setLayout(new GridLayout(13, 13, 1, 1));
+		panel.setMaximumSize(new Dimension ((int) 50, (int) 50));
+		frmPokerbluffing.getContentPane().add(panel, BorderLayout.CENTER);
+		JPanel bottom = new JPanel();  
+		bottom.setLayout(new GridLayout(0, 1, 0, 0));	
+		textField = new JTextField();
+		textField.setColumns(10);
+		bottom.add(textField);
+		frmPokerbluffing.getContentPane().add(bottom, BorderLayout.SOUTH);
+		String name;
+		String style;
+		ArrayList <String> s=new ArrayList<>();
+		for ( int i = 0; i < rank.length; i++ ) {
+			for ( int j = 0; j < rank.length; j++ ) {
+				if ( i == j ) {
+					name = rank[i] + rank[j];
+		    		style = "pair";
+				} else if ( i < j ) {
+					name = rank[i] + rank[j] + "s";
+					style = "s";
+				} else { // if ( i > j ) 
+					name = rank[j] + rank[i] + "o";
+					style = "o";
+				}
+				range = new JToggleButton(name);
+				if (style == "pair") {
+					range.setBackground(Color.white);
+				}else if(style=="s") {
+					range.setBackground(Color.cyan);
+				}else if(style=="o") {
+					range.setBackground(Color.magenta);
+				}
+				
+				ItemListener itemListener = new ItemListener() { 
+					  
+			            // itemStateChanged() method is nvoked automatically 
+			            // whenever you click or unlick on the Button. 
+					public void itemStateChanged(ItemEvent itemEvent) { 
+			  
+			                // event is generated in button 
+						int state = itemEvent.getStateChange(); 
+			  
+			                // if selected print selected in console 
+						if (state == ItemEvent.SELECTED) { 
+							String r=((AbstractButton) itemEvent.getSource()).getLabel();
+			          		s.add(r);
+			          		textField.setText(s.toString());
+						} else { 
+			                String r=((AbstractButton) itemEvent.getSource()).getLabel();
+			               	s.remove(s.indexOf(r));			                	
+			               	textField.setText(s.toString());
+						} 
+					} 
+				}; 
+				range.addItemListener(itemListener);
+				
+				panel.add(range);
+				}
+		}
+
+		JPanel east = new JPanel();
+		frmPokerbluffing.getContentPane().add(east, BorderLayout.EAST);
+		east.setLayout(new GridLayout(0, 1, 0, 0));
+		JLabel playercard = new JLabel("ENTER YOUR CARD HERE");
+		playercard.setFont(font1);
+		playercard.setHorizontalAlignment(SwingConstants.CENTER);
+		east.add(playercard);
+		JTextField player= new JTextField();
+		player.setFont(font1);
+		east.add(player);
+		player.setColumns(10);
+		  
+		  JLabel community = new JLabel("ENTER THE COMMUNITY CARD HERE");
+		  community.setHorizontalAlignment(SwingConstants.CENTER);
+		  community.setFont(font1);
+		  east.add(community);
+		  JTextField board = new JTextField();
+		  board.setFont(font1);
+		  east.add(board);
+		  board.setColumns(10);
+		  JLabel result = new JLabel("");
+		  result.setFont(font1);
+		  east.add(result);
+		  JButton enter = new JButton("ENTER");
+		  enter.setFont(font1);
+		  enter.addActionListener(new ActionListener() {
+			  public void actionPerformed(ActionEvent arg0) {
+					Deck deck=new Deck();
+					String playercard=player.getText();
+					String community =board.getText();
+					deck.remove(playercard);
+					deck.remove(community);
+					double answer=deck.bluffing(s, community);
+					result.setText("<html>The cold bluffing percentage is: <br>"+Double.toString(answer)+" %</html>");
+			  }
+		  });
+		  bottom.add(enter);
+	}
+}
+
